4bb85a72c40ba4601b7475fc7f1df828dca26c3d
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-02 22:55:43 -0400

    Mostrar posicion final I

--- src/js/presenter.js
+++ src/js/presenter.js
@@ -1,4 +1,4 @@
-import validarmatriz from "./validador.js"
+import {validarmatriz, validarposicion} from "./validador.js"
 const comandos = document.querySelector("#comandos");
 const ejecutar = document.querySelector("#ejecutar");
 const resultado = document.querySelector("#resultado");
@@ -23,11 +23,13 @@ ejecutar.addEventListener("click", (event) => {
     const columna=parseInt(dimensiones[1]);
 
     const validacion=validarmatriz(fila,columna);
+    const posicionfinal=validarposicion(resultadocomando);
    
     resultado.innerHTML =`
     <p><span id="subtitulos">Tamaño Matriz</span>: ${validacion.mensajevalidacion}</p>
     <p><span id="subtitulos"> Posición Inicial: </span><span id="respuestas"> ${letra}</span></p>
     <p><span id="subtitulos">Comandos:</span><span>${avance}</span></p>
+    <p><span id="subtitulos">Posición Final:</span><span>${posicionfinal}</span></p>
     
     `;
     errores.innerHTML =`<p><span id="subtitulos"></span>${validacion.mensajeinvalidacion}</p>`;
