31b1f3f43666b89716426ec1bf62f1da62125f18
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-02 22:50:46 -0400

    Ejecutar un comando I

--- src/js/auto.test.js
+++ src/js/auto.test.js
@@ -1,10 +1,13 @@
-import validarmatriz from "./validador";
+import {validarmatriz, validarposicion} from "./validador";
 
 describe("Validar tamaño de la matriz < 20x20",()=>{
     it("Deberia mostrar que es menor a 20x20",()=>{
         expect(validarmatriz(18,18)).toEqual({fila:18,columna:18,mensajeinvalidacion:"",mensajevalidacion:"18 X 18"});
     });
 
+    it("Validar la posición final del primer comando (I)",()=>{
 
+        expect(validarposicion('5,5/1,2N/I')).toEqual('1,2O');
+    });
 
 });
--- src/js/validador.js
+++ src/js/validador.js
@@ -14,4 +14,38 @@ function validarmatriz(fila,columna){
     
 }
 
-export default validarmatriz;
+function validarposicion(comando) {
+    const [, coordenadas, listaComandos] = comando.match(/(\d+,\d+[NSEOW])(.*)/);
+  
+    const [x, y, dirección] = coordenadas.match(/(\d+),(\d+)([NSEOW])/).slice(1);
+  
+    // Iniciar las coordenadas x, y, que sean enteros.
+    const xInicial = parseInt(x);
+    const yInicial = parseInt(y);
+    // Iniciar las coordenadas finales
+    let xFinal = xInicial;
+    let yFinal = yInicial;
+    let orientacion = dirección; // Almacenar la orientación en la variable orientacion
+  
+    for (const comando of listaComandos) {
+      if (comando === 'I')
+       {
+        // Cambiar el giro a la derecha en lugar de la izquierda
+        if (orientacion === 'N') {
+          orientacion = 'O';
+        } else if (orientacion === 'O') {
+          orientacion = 'S';
+        } else if (orientacion === 'S') {
+          orientacion = 'E';
+        } else if (orientacion === 'E') {
+          orientacion = 'N';
+        }
+      }
+    }
+  
+    const posicionFinal = `${xFinal},${yFinal}${orientacion}`;
+  
+    return posicionFinal;
+  }
+
+export {validarmatriz, validarposicion};
